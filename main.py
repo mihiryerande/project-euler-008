66a658d6e2e79d0377aebbffdf2ba0c43cef582a
Mihir Yerande <51039100+mihiryerande@users.noreply.github.com>
2022-03-16 15:31:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,9 @@
 #
 #     Find the thirteen adjacent digits in the 1000-digit number that have the greatest product.
 #     What is the value of this product?
-import operator
+
 from functools import reduce
+import operator
 
 BIG_NUM_STR = ''.join([
     '73167176531330624919225119674426574742355349194934',
